af4b43d17e06060c6fce499df80d853612398941
unknown <digh5420@naver.com>
2022-07-12 17:26:28 +0900

    20220712

---
+++ Unity0712_4/Assets/PipeObjectMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeObjectMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(transform.position.x - 0.03f, transform.position.y, 0);
+        if (transform.position.x <= -7.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    /*
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x - 0.03f, transform.position.y, 0);
+        if (transform.position.x <= -7.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+    */
+}
